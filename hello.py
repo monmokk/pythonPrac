7ced2010ce0661ddde4807afc6f08c4ca99e79ea
mjk <myeongjikang910814@gmail.com>
2022-04-22 01:35:47 +0900

    py

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
+fruits = ['사과', '배', '배', '감', '수박', '귤', '딸기', '사과', '배', '수박']
+people = [{'name': 'bob', 'age': 20},
+          {'name': 'carry', 'age': 38},
+          {'name': 'john', 'age': 7},
+          {'name': 'smith', 'age': 17},
+          {'name': 'ben', 'age': 27}]
 
+print(fruits)
+
+def sum(a, b):
+    return a+b
+result = sum(1,2)
+print(result)
