0c7bcf9fb66a274439fdb22cf4aaca9ac491af62
mjk <myeongjikang910814@gmail.com>
2022-04-22 01:42:28 +0900

    py 문법

--- hello.py
+++ hello.py
@@ -5,9 +5,22 @@ people = [{'name': 'bob', 'age': 20},
           {'name': 'smith', 'age': 17},
           {'name': 'ben', 'age': 27}]
 
-print(fruits)
+for fruit in fruits:
+    print(fruit)
+
+for person in people:
+    if person['age'] > 20:
+        print(person['name'])
 
 def sum(a, b):
     return a+b
 result = sum(1,2)
 print(result)
+
+def is_adult(age):
+    if age > 20:
+        print('성인입니다')
+    else:
+        print('청소년입니다')
+
+is_adult(30)
